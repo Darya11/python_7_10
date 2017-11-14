43977609041207359c931ec67322bd468ca6ec35
Darya11 <daniellaivasiuk@gmail.com>
2017-11-14 20:39:25 +0200

    initial commit

--- hw_7_10.py
+++ hw_7_10.py
@@ -1,36 +1,34 @@
 #task_7
 #-----------------------------------------------
-a = '05.17.2016'
-print(a)
-a = ['05.', '17.', '2016']
-a[0], a[1] = a[1], a[0]
-b = "".join(a)
-print(b)
+date_us = '05.17.2016'
+print(date_us)
+date_eu = date_us.split(".")
+print(date_eu[1] + '.' + date_eu[0] + '.' + date_eu[2])
 
 
 #task_8
 #------------------------------------------------
-k = 'Mark Zuckerberg'
-print(k)
-lst = k.split(' ')
-print(lst[1] + ' ' + lst[0])
+name_surname = 'Mark Zuckerberg'
+print(name_surname)
+surname_name = name_surname.split(' ')
+print(surname_name[1] + ' ' + surname_name[0])
 
 
 #task_9
 #------------------------------------------------
-s = 'employee_first_name'
-print(s)
-s ='{0}mployee{1}irst{2}ame'.format('E', 'F', 'N')
-print(s)
+snake_style = 'employee_first_name'
+print(snake_style)
+camelized_style = snake_style.split('_')
+print(camelized_style[0].capitalize() + camelized_style[1].capitalize() + camelized_style[2].capitalize())
 
 
 #task_10
 #-------------------------------------------------
-m ='Leo Tolstoy*1828-08-28*1910-11-20'.split('*')
-born = m[1].split("-")
-dead = m[2].split("-")
-lives = int(dead[0]) - int(born[0])
-print(m[0] + ' ' + str(lives))
+Leo_Tolstoy ='Leo Tolstoy*1828-08-28*1910-11-20'.split('*')
+birth_date = Leo_Tolstoy[1].split("-")
+death_date = Leo_Tolstoy[2].split("-")
+age = int(death_date[0]) - int(birth_date[0])
+print(Leo_Tolstoy[0] + ' ' + str(age))
 
 
 
