f35727afaa41f00971af1a32af8ec2286f4531c4
Darya11 <daniellaivasiuk@gmail.com>
2017-11-19 14:51:01 +0200

    initial commit

--- hw_14_16.py
+++ hw_14_16.py
@@ -27,3 +27,21 @@ def circus_intersections (x1, x2, r1, y1, y2, r2):
 
 
 result1 = circus_intersections(7, 8, 3, 8, 9, 5)
+
+
+#task_16
+#---------------------------------------------
+print("Два поезда движутся с заданной скоростью. Между ними 10 км. Первый поезд через 4 км может свернуть с пути."
+      "Столкнутся ли поезда?")
+
+
+def trains_crash (velocity1, velocity2):
+    square = 10
+    time_train_1 = (10 - 6)/velocity1
+    time_train_2 = (10 - 4)/velocity2
+    if time_train_1 < time_train_2:
+        print('Поезда не столкнутся')
+    else:
+        print("Поезда столкнутся")
+
+result2 = trains_crash(30, 60)
