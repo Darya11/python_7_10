a8aa34a6c186e76a3eba14970129e422aa93dab7
Darya11 <daniellaivasiuk@gmail.com>
2017-11-20 16:21:02 +0200

    initial commit

--- hw_11_13.py
+++ hw_11_13.py
@@ -3,11 +3,13 @@ import math
 #task_11
 #---------------------------------
 
-def degrees_to_radians (value1, value2, value3):
-   return math.cos(math.radians(value1)), math.cos(math.radians(value2)), math.cos(math.radians(value3))
+def degrees_to_radians (value):
+   return math.cos(math.radians(value))
 
-result1, result2, result3 = degrees_to_radians (60, 45, 40)
-print('Значение косинусов улов в 60, 45, 40 градусов в радианах равно: %.1f' % result1,
+result1 = degrees_to_radians (60)
+result2 = degrees_to_radians(45)
+result3 = degrees_to_radians(40)
+print('Значение косинусов улов в градусах будет равно в радианах: %.1f' % result1,
       '%.1f' % result2, '%.1f' % result3)
 
 #task_12
@@ -23,13 +25,13 @@ print('Сумма цифр трехзначного числа равна:', res
 
 #task_13
 #----------------------------------
-def right_triangle(cathetus_1, cathetus_2):
+def right_triangle_perimeter_and_square(cathetus_1, cathetus_2):
     hypotenuse = math.sqrt(pow(cathetus_1, 2) + pow(cathetus_2, 2))
     triangle_perimeter = hypotenuse + cathetus_1 + cathetus_2
     triangle_square = 1/2 * cathetus_1*cathetus_2
     return triangle_perimeter, triangle_square
 
-result_1, result_2 = right_triangle(5,6)
+result_1, result_2 = right_triangle_perimeter_and_square(5,6)
 print('Периметр и площадь прямоугольного треугольника равно: ' '%.1f'% result_1, 'см. и', int(result_2), 'см.')
 
 
