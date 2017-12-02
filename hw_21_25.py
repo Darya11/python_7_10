7f84907c532b34936ce60315d4f74709fd799a95
Darya11 <daniellaivasiuk@gmail.com>
2017-12-02 16:17:58 +0200

    initial commit

--- hw_21_25.py
+++ hw_21_25.py
@@ -2,30 +2,19 @@ import random
 #task_21
 #-----------------------------------
 
-def get_number_of_twelwe_digits(num_limited, lower_bound, upper_bound):
-    number = 0
-
-    for i in range(num_limited):
-        random_number = random.randint(lower_bound, upper_bound)
-        number = random_number
-    return number
-
-
-number = get_number_of_twelwe_digits(1, 100000000000, 1000000000000)
-print(number)
+number = random.randint(100000000000, 999999999999.)
 
 
 def get_max_digit(number):
-    number_of_twelwe_digits = number
-    max_digit = number_of_twelwe_digits % 10
-    number_of_twelwe_digits = number_of_twelwe_digits // 10
-    while number_of_twelwe_digits > 0:
-        if number_of_twelwe_digits % 10 > max_digit:
-            max_digit = number_of_twelwe_digits % 10
-        number_of_twelwe_digits = number_of_twelwe_digits // 10
+    max_digit = number % 10
+    number = number // 10
+    while number > 0:
+        if number % 10 > max_digit:
+            max_digit = number % 10
+        number = number // 10
     return max_digit
 
 
 result2 = get_max_digit(number)
 print(result2)
-print('The max digit in radom twelwe-digits number %d' % number, 'is: %d' % result2)
+print('The max digit in random twelve-digits number %d' % number, 'is: %d' % result2)
