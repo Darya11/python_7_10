a04dee40e31e4f532ae488451943d42dad722de0
Darya11 <daniellaivasiuk@gmail.com>
2017-12-08 23:58:48 +0200

    initial commit

--- hw_21_25.py
+++ hw_21_25.py
@@ -1,5 +1,6 @@
 import random
 import string
+import math
 #task_21
 #-----------------------------------
 
@@ -36,3 +37,57 @@ result = group_by_surname(['Will Smith', 'Robin Clarks', 'Ivan Tray', 'Brian Mel
 
 print('The quantity of students in groups are: '
       'A-I: %d' % result[0], 'J-K: %d' % result[1] , 'Q-T: %d' % result[2], 'U-Z: %d' % result[3])
+
+
+#task_23
+#------------------------------------
+
+random_number = random.randint(1, 10)
+print(random_number)
+
+while True:
+    user_choice = input()
+
+    valid_input = True
+    if not user_choice.isnumeric():
+        valid_input = False
+
+    elif not (1 <= int(user_choice) <= 10):
+        valid_input = False
+
+    if not valid_input:
+        print('Press the number from 1 to 10')
+        continue
+
+    user_choice = int(user_choice)
+    if user_choice > random_number:
+        print("The number is smaller, try again")
+
+    elif user_choice < random_number:
+        print("The number is bigger, try again")
+
+    elif user_choice == random_number:
+        print("You have guessed the number")
+        break
+
+
+
+#task_24
+#------------------------------------------------
+
+def chess_reward():
+    quantaty_of_corn_more_million = 1
+
+    while quantaty_of_corn_more_million < 1000000:
+        quantaty_of_corn_more_million *= 2
+
+    quantaty_of_corn_more_million = quantaty_of_corn_more_million - 1
+    cell_of_chess = math.log2(quantaty_of_corn_more_million)
+
+
+    return round(cell_of_chess), quantaty_of_corn_more_million
+
+
+result = chess_reward()
+print('The number of cell from what the quantaty of corns is more million: %d' % result[0],
+      ' and the total amount of corns is: %d' % result[1])
