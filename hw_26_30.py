ff0e89349cd3df6a60e82b2752ac8201df1e0cee
Darya11 <daniellaivasiuk@gmail.com>
2017-12-10 22:57:19 +0200

    initial commit

---
+++ hw_26_30.py
@@ -0,0 +1,23 @@
+import random
+list_of_random_digits = [random.randint(-1, 1) for x in range(11)]
+
+print('Random list is: %s' % list_of_random_digits)
+
+def calc_frequency(list_of_random_digits):
+    frequency_of_minus_1 = sum([1 for elem in list_of_random_digits if elem == -1])
+    frequency_of_0 = sum([1 for elem in list_of_random_digits if elem == 0])
+    frequency_of_1 = sum([1 for elem in list_of_random_digits if elem == 1])
+    list_of_frequences = [frequency_of_minus_1, frequency_of_0, frequency_of_1]
+    print('Frequency calculation of numbers in random list %s' % list_of_random_digits,'is: %s' % list_of_frequences)
+    print('Max number of frequency is:')
+
+    for i in list_of_frequences:
+        if list_of_frequences[0] != list_of_frequences[1] and list_of_frequences[0] != list_of_frequences[2]\
+                and list_of_frequences[1] != list_of_frequences[2]:
+            return max(list_of_frequences)
+        else:
+            return None
+
+
+result_1 = calc_frequency(list_of_random_digits)
+print(result_1)
